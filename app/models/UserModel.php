d7b79e34b8e85604b332d45bb14d7c213e18ca02
Laurence Aldovino <laurencedgreat02@gmail.com>
2025-01-09 23:29:01 +0800

    Update UserModel.php

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -10,7 +10,7 @@ class UserModel extends Model {
     public function get_user_by_id($id) {
         return $this->db->table('Users')->where('id', $id)->get();
     }
-
+  
     public function add_user($data) {
         $bind = [
             'name' => $data['name'],
